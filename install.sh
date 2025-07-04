f37e8d35b1ee5de212299c532f11bb22c42e3a50
Le Minh Thin <minhthinle69@gmail.com>
2025-07-04 15:14:47 +0700

    Added a few programs

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-configs=("kew"  "nvim"  "waybar"  "zathura"  "kitty"  "sway"  "yazi")
+# Links and shits
+configs=("kew"  "nvim"  "waybar"  "zathura"  "kitty"  "sway"  "yazi" "rofi" "hypr")
 
 for app in ${configs[@]}; do
     ln -sf $(pwd)/$app $HOME/.config/
@@ -8,3 +9,11 @@ done
 
 ln -sf $(pwd)/starship.toml $HOME/.config/
 ln -sf $(pwd)/.zshrc $HOME/
+sudo ln -sf $(pwd)/environment /etc/
+
+# GTK Theme
+cd ..
+git clone https://github.com/Fausto-Korpsvart/Gruvbox-GTK-Theme
+cd Gruvbox-GTK-Theme
+./install.sh
+gsettings set org.gnome.desktop.interface gtk-theme Gruvbox-Dark
