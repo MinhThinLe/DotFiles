281a36b410ab4d89b6f783d53fcfc0441fd94b4c
LeMinhThin <minhthinle69@gmail.com>
2025-07-11 21:41:09 +0700

    Sway config renovation and also switch out swayidle for hypridle I guess

--- rofi/applets/bin/calculator.sh
+++ rofi/applets/bin/calculator.sh
@@ -1,4 +1,12 @@
 dir="$HOME/.config/rofi/launchers/type-1"
 theme='style-5'
 
-rofi -show calc -modi calc -no-show-match -no-sort -theme ${dir}/${theme}
+rofi -show calc \
+    -modi calc \
+    -no-show-match \
+    -no-sort \
+    -no-bold \
+    -theme ${dir}/${theme} \
+    -kb-accept-entry 'Control+Escape' \
+    -kb-accept-custom 'Return' \
+    -calc-command "wl-copy {result}"
