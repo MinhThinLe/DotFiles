6260888a97129966a35bcde84ccaa39722c80fd1
LeMinhThin <minhthinle69@gmail.com>
2025-07-14 11:13:53 +0700

    Notification makeover

--- rofi/applets/bin/clipboard.sh
+++ rofi/applets/bin/clipboard.sh
@@ -2,6 +2,6 @@ source "$HOME"/.config/rofi/applets/shared/theme.bash
 theme="$type/$style"
 
 cliphist list |\
-    rofi -dmenu -display-columns 2 -theme ${theme} |\
+    rofi -dmenu -display-columns 2 -theme ${theme} -p "Clipboard history" |\
     cliphist decode |\
     wl-copy
