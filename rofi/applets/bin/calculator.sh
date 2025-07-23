ef87f6fff2ce5485adc93cc5e91c4704011eddef
LeMinhThin <minhthinle69@gmail.com>
2025-07-23 17:47:40 +0700

    Display config and calculator

--- rofi/applets/bin/calculator.sh
+++ rofi/applets/bin/calculator.sh
@@ -6,7 +6,9 @@ rofi -show calc \
     -no-show-match \
     -no-sort \
     -no-bold \
+    -no-history \
     -theme ${dir}/${theme} \
     -kb-accept-entry 'Control+Return' \
     -kb-accept-custom 'Return' \
-    -calc-command "printf '{result}' | wl-copy -t 'text/plain;charset=utf-8'"
+    -calc-command "printf '{result}' | wl-copy -t 'text/plain;charset=utf-8'" \
+    -theme-str "listview { lines: 0; } entry { placeholder: \"Taper votre expression...\";}"
