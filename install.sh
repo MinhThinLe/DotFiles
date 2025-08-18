e79479b8774b599423f4f6868399aea7865667da
LeMinhThin <minhthinle69@gmail.com>
2025-08-18 18:57:31 +0700

    Removed qt5ct color file to reflect changes made

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 # Links and shits
-configs=("kew" "nvim" "waybar" "zathura" "kitty" "sway" "yazi" "rofi" "hypr" "mako" "kanshi")
+apps=("kew" "nvim" "waybar" "zathura" "kitty" "sway" "yazi" "rofi" "hypr" "mako" "kanshi")
 
-for app in ${configs[@]}; do
+for app in ${apps[@]}; do
     ln -sf $(pwd)/$app $HOME/.config/
 done
 
 ln -sf $(pwd)/starship.toml $HOME/.config/
 ln -sf $(pwd)/.zshrc $HOME/
-ln -sf $(pwd)/qtxct/gruvbox.conf $HOME/.config/qt5ct/colors/gruvbox.conf
 ln -sf $(pwd)/qtxct/gruvbox.conf $HOME/.config/qt6ct/colors/gruvbox.conf
 sudo ln -sf $(pwd)/environment /etc/
 
