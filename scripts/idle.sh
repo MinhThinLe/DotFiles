d8ecacc471169f76aef9c36b20657a86c00506f0
Lê Minh Thìn <minhthinle69@gmail.com>
2025-11-10 21:07:57 +0700

    Removed idle.sh as it's no longer necessary

--- scripts/idle.sh
+++
@@ -1,7 +0,0 @@
-swayidle \
-    lock "pidof hyprlock || hyprlock -q" \
-    unlock 'pkill hyprlock' \
-    timeout 300 "loginctl lock-session" \
-    timeout 480 'niri msg action power-off-monitors' \
-    timeout 600 'systemctl suspend' \
-    before-sleep "loginctl lock-session"
