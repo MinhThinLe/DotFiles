91d8925620fd4f59b13bd7cf68c3f136ea12a3ac
Lê Minh Thìn <minhthinle69@gmail.com>
2025-12-14 11:43:54 +0700

    Feat: nvim tabbar

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -18,7 +18,7 @@ vim.cmd("language en_US.utf8")
 vim.cmd("colorscheme gruvbox")
 
 require 'typst-preview'.setup {
-    open_cmd = "chromium %s"
+    open_cmd = "firefox --new-window %s"
 }
 
 local rule = require('nvim-autopairs.rule')
@@ -49,8 +49,27 @@ require("lualine").setup {
 
 require("oil").setup()
 
---
--- require('render-markdown').setup({
---     render_modes = true,
---     preset = 'obsidian',
--- })
+vim.o.tabline = "%!v:lua.Tabline()"
+
+vim.api.nvim_set_hl(0, "TabLine", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE" })
+
+vim.api.nvim_set_hl(0, "TabLinePillActive",   { fg = "#edbbb2", bg = "#98971a" })
+vim.api.nvim_set_hl(0, "TabLinePillInactive", { fg = "#ebdbb2", bg = "#928374" })
+
+function _G.Tabline()
+  local s = ""
+  local tabs = vim.api.nvim_list_tabpages()
+  local current = vim.api.nvim_get_current_tabpage()
+
+  for i, tab in ipairs(tabs) do
+	local is_active = (tab == current)
+
+	local hl_text  = is_active and "%#TabLinePillActive#"   or "%#TabLinePillInactive#"
+    
+	s = s .. hl_text .. " " .. i .. " "
+	s = s .. "%#TabLine# "
+  end
+
+  return s
+end
