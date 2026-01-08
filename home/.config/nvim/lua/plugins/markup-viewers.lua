10fc2e1f87a4115b3f518389a273a17f30e42679
T0ast <minhthinle69@gmail.com>
2026-01-08 09:29:10 +0700

    Added markview.nvim

--- home/.config/nvim/lua/plugins/markup-viewers.lua
+++ home/.config/nvim/lua/plugins/markup-viewers.lua
@@ -7,4 +7,23 @@ return {
             open_cmd = "zen-browser --new-window %s"
         }
     },
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false,
+        opts = {
+            html = {
+                enabled = false,
+            },
+            latex = {
+                enabled = false,
+            },
+            typst = {
+                enabled = false,
+            },
+            preview = {
+                icon_provider = "nvim-web-devicons",
+                -- modes = { "n", "no", "c", "v", "i" },
+            }
+        }
+    }
 }
