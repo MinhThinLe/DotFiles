c77f33401ec27ad14b0801272f515687ee5b18b1
T0ast <minhthinle69@gmail.com>
2026-02-06 21:14:19 +0700

    Updated install script

--- scripts/executable_install-pkgs.sh
+++ scripts/executable_install-pkgs.sh
@@ -1,6 +1,10 @@
 sudo pacman -S --needed $(cat ~/scripts/pkg-list.txt)
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
+sudo pacman -S --needed rustup
+
+rustup default stable
+
+git clone https://aur.archlinux.org/paru.git
+cd paru
 makepkg -si
-yay -S --needed $(cat ~/scripts/pkg-list-aur.txt)
+paru -S --needed $(cat ~/scripts/pkg-list-aur.txt)
